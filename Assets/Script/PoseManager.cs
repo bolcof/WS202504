f69112feec9dc26c79e3507f40da677b3f0e22c8
bolcof <sanctum.crusade.ex@gmail.com>
2025-04-11 19:10:39 +0900

    ボタン設置

--- Assets/Script/PoseManager.cs
+++ Assets/Script/PoseManager.cs
@@ -6,19 +6,26 @@ using TMPro;
 
 public class PoseManager : MonoBehaviour {
 
-    [SerializeField] private List<GameObject> jointSphereList = new List<GameObject>();
+    public List<Transform> joints;
+    public PoseLibrary poseLibrary; // 保存先（ScriptableObject）
 
-    public struct PoseData {
-        string poseName;
-        //rotatelist?
-
-    }
-    private List<Quaternion> jointDataList = new List<Quaternion>();
-
-    public void PushSavePose() {
-
-        for(int i=0;i<jointSphereList.Count; i++) {
-
+    public void PushSavePose(string newPoseName) {
+        if (poseLibrary == null) {
+            Debug.LogError("PoseLibraryがアサインされていません！");
+            return;
         }
+
+        Pose pose = new Pose(newPoseName);
+        foreach (var joint in joints) {
+            pose.AddJointRotation(joint.localRotation);
+        }
+
+        poseLibrary.AddPose(pose);
+        Debug.Log($"Pose '{newPoseName}' を保存しました。");
+
+#if UNITY_EDITOR
+        UnityEditor.EditorUtility.SetDirty(poseLibrary); // ScriptableObjectの変更をEditorに認識させる
+        UnityEditor.AssetDatabase.SaveAssets();          // 保存
+#endif
     }
 }
